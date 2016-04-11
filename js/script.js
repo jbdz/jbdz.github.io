242f82a937145e29bfce0c5854fcc2a4515bdbd9
jorisb <joris.bordaz@gmail.com>
2016-04-11 21:21:22 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(function() {
+	$('a.page-scroll').bind('click', function(event) {
+		var $anchor = $(this);
+		$('html, body').stop().animate({
+			scrollTop: $($anchor.attr('href')).offset().top
+		}, 1200, 'easeInOutExpo');
+		event.preventDefault();
+	});
+});
